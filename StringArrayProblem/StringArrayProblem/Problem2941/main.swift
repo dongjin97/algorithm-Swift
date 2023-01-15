f7d6e027d9ea72f4b0cbc9164bac2fa420478597
wondongjin <82082770+dongjinwon-4507@users.noreply.github.com>
2023-01-15 19:31:50 +0900

    sol Problem 2941-baekjoon

---
+++ StringArrayProblem/StringArrayProblem/Problem2941/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  Problem2941
+//
+//  Created by 원동진 on 2023/01/15.
+//
+
+//import Foundation
+//var input = readLine()!
+//let arr = ["c=","c-","dz=","d-","lj","nj","s=","z="]
+//
+//for i in arr {
+//    input = input.replacingOccurrences(of: i, with: "*")
+//}
+//let result = Array(input).map{String($0)}
+//print("\(result.count)")
